09fce143f4ce3effe2fe1f2a79302ab73fb2733f
Antoine Cheron <cheron.antoine@gmail.com>
2019-04-11 15:34:50 +0200

    removed useless json readers that should have been removed earlier

--- src/main/java/com/github/antoinecheron/hypermedia/noannotation/json/reader/ApplicationReader.java
+++
@@ -1,9 +0,0 @@
-package com.github.antoinecheron.hypermedia.noannotation.json.reader;
-
-import java.util.Optional;
-
-public interface ApplicationReader<T> {
-
-  Optional<T> read(String input);
-
-}
